9ee5797d7344a9d638b8cfd32f3f4b887873ca74
Мальков Олег <karielka@yandex.ru>
2026-02-24 00:09:43 +0300

    Init db

---
+++ migrations/20260223210612_migration.down.sql
@@ -0,0 +1,17 @@
+drop table if exists account_session;
+drop table if exists payment;
+drop table if exists friend;
+drop table if exists watch_progress_episode;
+drop table if exists user_interaction;
+drop table if exists film_to_selection;
+drop table if exists selection;
+drop table if exists actor_to_film;
+drop table if exists genre_to_film;
+drop table if exists actor;
+drop table if exists episode;
+drop table if exists film;
+drop table if exists country;
+drop table if exists language;
+drop table if exists genre;
+
+drop function if exists set_updated_at();
