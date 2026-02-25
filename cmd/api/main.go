db5f55220bca2fad172372ea61049fa8d2e951d6
Saucesamba <arhipartyom@yandex.ru>
2026-02-25 14:44:39 +0300

    add yaml parsing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"github.com/go-park-mail-ru/2026_1_VKino/config"
+	"log"
+)
 
 func main() {
-	log.Println("hello, world")
+	cfg := config.Config{}
+	err := cfg.LoadConfig()
+	if err != nil {
+		log.Fatalf("Unable to load config %w", err)
+	} else {
+		log.Println("Config loaded successfully")
+	}
+
 }
